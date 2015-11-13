d1f1ce62952c800b2c4d8609876e41661e3106f5
Hoct <hoct@hoctdemacbook-pro.local>
2015-11-13 12:42:16 +0800

    Initial Commit

---
+++ TestDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TestDemo
+//
+//  Created by Hoct on 15/11/13.
+//  Copyright © 2015年 Hock. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
